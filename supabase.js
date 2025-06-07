14ff6372c6471b545d6ca9fc7cf3ee6540b730c1
C-Jerome-Emmanuel <c.jerome.emmanuel@gmail.com>
2025-06-07 16:11:20 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-export const supabase = createClient(
-  'https://YOUR-PROJECT.supabase.co',
-  'YOUR-PUBLIC-ANON-KEY'
-);
